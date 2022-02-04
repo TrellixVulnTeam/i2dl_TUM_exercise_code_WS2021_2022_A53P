6f274e354fc51e355f4d7b8d4f2514a947504d4d
Ghiara <90917547+Ghiara@users.noreply.github.com>
2022-02-04 16:33:23 +0100

    Add files via upload

---
+++ exercise_11/exercise_code/rnn/text_classifiers.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+# from torch.nn.utils.rnn import pack_padded_sequence
+from torch.nn.utils.rnn import (
+    pack_padded_sequence,
+    pad_packed_sequence,
+    PackedSequence
+)
+import torch.nn.functional as F
+from .rnn_nn import Embedding, RNN, LSTM
+
+
+class RNNClassifier(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, hidden_size, use_lstm=True, **additional_kwargs):
+        """
+        Inputs:
+            num_embeddings: size of the vocabulary
+            embedding_dim: size of an embedding vector
+            hidden_size: hidden_size of the rnn layer
+            use_lstm: use LSTM if True, vanilla RNN if false, default=True
+        """
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.hidden_size = hidden_size
+        self.use_lstm = True
+
+        # Change this if you edit arguments
+        self.hparams = {
+            'num_embeddings': num_embeddings,
+            'embedding_dim': embedding_dim,
+            'hidden_size': hidden_size,
+            'use_lstm': use_lstm,
+            **additional_kwargs
+        }
+#         self.hparams = hparams
+#         self.num_embeddings = hparams['num_embeddings']
+#         self.embedding_dim = hparams['embedding_dim']
+#         self.hidden_size = hparams['hidden_size']
+#         self.use_lstm = hparams['use_lstm']
+#         self.batch_size = hparams['batch_size']
+
+        ########################################################################
+        # TODO: Initialize an RNN network for sentiment classification         #
+        # hint: A basic architecture can have an embedding, an rnn             #
+        # and an output layer                                                  #
+        ########################################################################
+        
+#         self.embed = Embedding(self.num_embeddings, self.embedding_dim, padding_idx=0)
+        self.embed = nn.Embedding(self.num_embeddings, self.embedding_dim)
+        
+        
+        self.lstm = nn.LSTM(self.embedding_dim, self.hidden_size)
+        self.rnn = nn.RNN(self.embedding_dim, self.hidden_size)
+        self.linear1 = nn.Linear(self.hidden_size, 1)
+        self.m = nn.Sigmoid()
+#         self.m = nn.Softmax(dim=1)
+#         self.linear2 = nn.Linear(self.hidden_size//2, 1)
+        
+        
+
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+
+#         return output
+
+    
+    def forward(self, sequence, lengths=None):
+        """
+        Inputs
+            sequence: A long tensor of size (seq_len, batch_size)
+            lengths: A long tensor of size batch_size, represents the actual
+                sequence length of each element in the batch. If None, sequence
+                lengths are identical.
+        Outputs:
+            output: A 1-D tensor of size (batch_size,) represents the probabilities of being
+                positive, i.e. in range (0, 1)
+        """
+
+        output = None
+
+        ########################################################################
+        # TODO: Apply the forward pass of your network                         #
+        # hint: Don't forget to use pack_padded_sequence if lenghts is not None#
+        # pack_padded_sequence should be applied to the embedding outputs      #
+        ########################################################################
+        seq_len, batch_size = sequence.shape # (10, 3) (batch_size = 3, 3, 1, 1, 1)
+        device = torch.device('cpu')
+        
+        x = self.embed(sequence)
+        if lengths is not None:
+            x = pack_padded_sequence(x, lengths)
+        
+        h_0 = torch.zeros(1, batch_size, self.hidden_size).to(device)
+        c_0 = torch.zeros(1, batch_size, self.hidden_size).to(device)
+
+#         _, out = self.rnn(x, h_0)
+        _, (out, _) = self.lstm(x, (h_0, c_0))
+        out = out[-1]
+#         print(out.shape)
+        out = self.linear1(out)
+        output = self.m(out).squeeze(1)
+#         print(output)
+        
+        
+        
+#         output = output.view(batch_size,)
+
+
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+        return output
+
+        
