b58526e80572f90365cbab53221d7155fb40fb43
Y.Meng <90917547+Ghiara@users.noreply.github.com>
2022-07-15 15:51:03 +0200

    Add files via upload

---
+++ exercise_10/exercise_code/networks/segmentation_nn.py
@@ -0,0 +1,117 @@
+"""SegmentationNN"""
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+
+class SegmentationNN(pl.LightningModule):
+
+    def __init__(self, num_classes=23, hparams=None):
+        super().__init__()
+        self.hparams = hparams
+        #######################################################################
+        #                             YOUR CODE                               #
+        #######################################################################
+
+        from torchvision import models
+        self.features = models.alexnet(pretrained=True).features
+#         self.features = models.resnet18(pretrained=True).create_feature_extractor()
+#         self.features = nn.Sequential(
+#             nn.Conv2d(3, 64, kernel_size=(11, 11), stride=(4, 4), padding=(2, 2)),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False),
+#             nn.Conv2d(64, 192, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2)),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False),
+#             nn.Conv2d(192, 384, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+#             nn.ReLU(inplace=True),
+#             nn.Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+#             nn.ReLU(inplace=True),
+#             nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+#             nn.ReLU(inplace=True),
+#             nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False)
+#         )
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+#             nn.Conv2d(256, 4096, kernel_size=1, padding=0),
+            nn.Conv2d(256, 4096, kernel_size=1, padding=0),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+#             nn.Conv2d(4096, 1024, kernel_size=1, padding=0),
+#             nn.ReLU(inplace=True),
+#             nn.Dropout(),
+#             nn.Conv2d(1024, 256, kernel_size=1, padding=0),
+#             nn.ReLU(inplace=True),
+#             nn.Dropout(),
+#             nn.Conv2d(2048, 256, kernel_size=1, padding=0),
+#             nn.ReLU(inplace=True),
+#             nn.Dropout(),
+            nn.Conv2d(4096, 256, kernel_size=1, padding=0),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Conv2d(256, num_classes, kernel_size=1, padding=0),
+            nn.Upsample(scale_factor=40),
+            nn.Conv2d(num_classes, num_classes, kernel_size=3, padding=1),
+        )
+
+        #######################################################################
+        #                           END OF YOUR CODE                          #
+        #######################################################################
+
+    def forward(self, x):
+        """
+        Forward pass of the convolutional neural network. Should not be called
+        manually but by calling a model instance directly.
+
+        Inputs:
+        - x: PyTorch input Variable
+        """
+        #######################################################################
+        #                             YOUR CODE                               #
+        #######################################################################
+
+        x = self.features(x)
+        x = self.classifier(x)
+
+        #######################################################################
+        #                           END OF YOUR CODE                          #
+        #######################################################################
+
+        return x
+
+    @property
+    def is_cuda(self):
+        """
+        Check if model parameters are allocated on the GPU.
+        """
+        return next(self.parameters()).is_cuda
+
+    def save(self, path):
+        """
+        Save model with its parameters to the given path. Conventionally the
+        path should end with "*.model".
+
+        Inputs:
+        - path: path string
+        """
+        print('Saving model... %s' % path)
+        torch.save(self, path)
+
+        
+class DummySegmentationModel(pl.LightningModule):
+
+    def __init__(self, target_image):
+        super().__init__()
+        def _to_one_hot(y, num_classes):
+            scatter_dim = len(y.size())
+            y_tensor = y.view(*y.size(), -1)
+            zeros = torch.zeros(*y.size(), num_classes, dtype=y.dtype)
+
+            return zeros.scatter(scatter_dim, y_tensor, 1)
+
+        target_image[target_image == -1] = 1
+
+        self.prediction = _to_one_hot(target_image, 23).permute(2, 0, 1).unsqueeze(0)
+
+    def forward(self, x):
+        return self.prediction.float()
